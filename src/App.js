f85c3d21b53fd7f5ba6e82157cea6bd77f59e53c
bp-skhan113 <skhan4@xavient.com>
2018-07-22 13:18:35 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./App.css";
+import {hot} from "react-hot-loader";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1> Hello, World! Testing jsdzfhuzd ah 8q 47jg11111111 </h1>
+                <h1> Hello, World! Testing jsdzfhuzd ah 8q 47jg22222222 </h1>
+                <h1> Hello, World! Testing jsdzfhuzd ah 8q 47jg33333333 </h1>
+            </div>
+        );
+    }
+}
+
+export default hot(module)(App);
